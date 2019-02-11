5f64b3166dfdc6f09e23e10615b62d0a8505b0fc
KonstantinRakovskij <34558479+KonstantinRakovskij@users.noreply.github.com>
2019-02-11 19:52:17 +0100

    Semi Final Updates

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,52 +6,50 @@
 
 
     //---------------Focus Contact2---------------- //
-    $('.input').each(function(){
-        $(this).on('blur', function(){
-            if($(this).val().trim() != "") {
+    $('.input').each(function () {
+        $(this).on('blur', function () {
+            if ($(this).val().trim() != "") {
                 $(this).addClass('has-val');
             }
             else {
                 $(this).removeClass('has-val');
             }
-        })    
+        })
     })
-            
-  
-    
+
     //---------Validate----------------//
-    var name = $('.validate-input input[name="name"]');
+    var name = $('.validate-input input[name="client_name"]');
     var email = $('.validate-input input[name="email"]');
     var message = $('.validate-input textarea[name="message"]');
 
 
-    $('.validate-form').on('submit',function(){
+    $('.validate-form').on('submit', function () {
         var check = true;
 
-        if($(name).val().trim() == ''){
+        if ($(name).val().trim() == '') {
             showValidate(name);
-            check=false;
+            check = false;
         }
 
 
-        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+        if ($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
             showValidate(email);
-            check=false;
+            check = false;
         }
 
-        if($(message).val().trim() == ''){
+        if ($(message).val().trim() == '') {
             showValidate(message);
-            check=false;
+            check = false;
         }
 
         return check;
     });
 
 
-    $('.validate-form .input').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-       });
+    $('.validate-form .input').each(function () {
+        $(this).focus(function () {
+            hideValidate(this);
+        });
     });
 
     function showValidate(input) {
@@ -66,27 +64,24 @@
         $(thisAlert).removeClass('alert-validate');
     }
 
-    
 })(jQuery);
 
-
 //-----Burger menu overlay------//
-$('#toggle').click(function() {
+$('#toggle').click(function () {
     $(this).toggleClass('active');
     $('#overlay').toggleClass('open');
-   });
-
+});
 
 //-------Scroll to Top------//
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+$(window).scroll(function () {
+    if ($(this).scrollTop() >= 50) {
+        $('#return-to-top').fadeIn(200);
     } else {
-        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+        $('#return-to-top').fadeOut(200);
     }
 });
-$('#return-to-top').click(function() {      // When arrow is clicked
+$('#return-to-top').click(function () {
     $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
+        scrollTop: 0
     }, 500);
 });
